9a2232876099d3629a1932a5a6b9a63e32f501ba
Zilas Jørgensen <Zilas2004@gmail.com>
2022-04-27 13:06:32 +0200

    hotfix

--- Test Code Project/Program.cs
+++ Test Code Project/Program.cs
@@ -2,67 +2,56 @@
 {
     public static void Main()
     {
-        double resultat = 0;
-        string tal1, tal2;
-        Console.WriteLine("[Velkommen til ConCalc]\n" +
-            "---------------------\n" +
-            "1. Plus\n" +
-            "2. Træk fra\n" +
-            "3. Gange\n" +
-            "4. Dividere\n" +
-            "5. Nettoløn Beregner" +
-            "---------------------\n" +
-            "Vælg en funktion");
-        string funktion = Console.ReadLine();
-        if (funktion != "5")
+        Console.WriteLine("Random Sort");
+        Console.Write("Enter elements of the array:");
+
+        var parts = Console.ReadLine().Split(new[] { "", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+        var array = new int[parts.Length];
+        for (int i = 0; i < parts.Length; i++)
         {
-            Console.Write("Tal 1: ");
-            tal1 = Console.ReadLine();
-            Console.Write("Tal 2: ");
-            tal2 = Console.ReadLine();
-            resultat = 0;
-            if (funktion == "1")
-            {
-                resultat = Convert.ToDouble(tal1) + Convert.ToDouble(tal2);
-            }
-            else if (funktion == "2")
-            {
-                resultat = Convert.ToDouble(tal1) - Convert.ToDouble(tal2);
-            }
-            else if (funktion == "3")
-            {
-                resultat = Convert.ToDouble(tal1) * Convert.ToDouble(tal2);
-            }
-            else
-            {
-                resultat = Convert.ToDouble(tal1) / Convert.ToDouble(tal2);
-            }
-        } else if (funktion == "5")
-        {
-            Console.Clear();
-            NettoBeregner();
-        } else
-        {
-            Console.WriteLine("Ugyldigt input");
+            array[i] = Convert.ToInt32(parts[i]);
         }
-        Console.WriteLine("---------------------\n" +
-            "Resultat\n" +
-            $"{resultat}\n" +
-            "---------------------\n" +
-            "Tryk ESC for at genstarte");
-        if (Console.ReadKey().Key == ConsoleKey.Escape)
-        {
-            Main();
-        } else
-        {
-            Console.WriteLine("Hav en god dag");
-        }
-    }
-    
-    public static double NettoBeregner()
-    {
-        Console.WriteLine();
-        return 0;
+
+        Console.WriteLine("Sorted array: {0}", string.Join(",", BogoSort(array)));
+
+        Console.ReadLine();
     }
 
+    static int[] Shuffle(int[] a)
+    {
+        Random random = new Random();
+        var n = a.Length;
+        while (n > 1)
+        {
+            n--;
+            var i = random.Next(n + 1);
+            var temp = a[i];
+            a[i] = a[n];
+            a[n] = temp;
+        }
+
+        return a;
+    }
+
+    static bool IsSorted(int[] a)
+    {
+        for (int i = 0; i < a.Length - 1; i++)
+        {
+            if (a[i] > a[i + 1])
+                return false;
+        }
+
+        return true;
+    }
+
+    static int[] BogoSort(int[] a)
+    {
+        while (!IsSorted(a))
+        {
+            a = Shuffle(a);
+        }
+
+        return a;
+    }
 }
+
