90551183d1f0e775e34ae90bc9e0b595db12d95c
Zilas Jørgensen <Zilas2004@gmail.com>
2022-05-17 15:02:48 +0200

    RaceGame added + hotfix

---
+++ RaceTrack/RaceGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace RaceTrack;
+
+class RaceGame
+{
+    #region Fullscreen Variables
+    [DllImport("kernel32.dll", ExactSpelling = true)]
+    private static extern IntPtr GetConsoleWindow();
+    private static IntPtr ThisConsole = GetConsoleWindow();
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    private const int HIDE = 0;
+    private const int MAXIMIZE = 3;
+    private const int MINIMIZE = 6;
+    private const int RESTORE = 9;
+    #endregion
+
+    public static void Main()
+    {
+        Console.Title = "Race Game";
+        Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+        ShowWindow(ThisConsole, MAXIMIZE);
+    }
+}
+
--- SchoolTask/student.cs
+++ SchoolTask/student.cs
@@ -19,6 +19,7 @@ class Student
 
     public List<string> Subjects { get; set; } = new List<string>();
 
-    public override string ToString() => $"\nID: {ID}\nName: {Name} \nAge: {Age} \nGender: {Gender} \nAdress: {Adress} \nEducation Line: {EdLine} \nClass: {Class} \nSubjects: " + String.Join(", ", Subjects) +"\n";
+    public override string ToString() => $"\nID: {ID}\nName: {Name} \nAge: {Age} \nGender: {Gender} \nAdress: {Adress} " +
+        $"\nEducation Line: {EdLine} \nClass: {Class} \nSubjects: " + String.Join(", ", Subjects) +"\n";
     //Overrides the string that will be displayed and will print it in the way I want it to. Using String interpolation to print it with the variables in it.
 }
