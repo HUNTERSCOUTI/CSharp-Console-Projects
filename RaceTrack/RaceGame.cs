ba641d7e6486e9156f29cd4b151f39ee90c0d79a
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-05-18 23:22:00 +0200

    Race Game Update #1

--- RaceTrack/RaceGame.cs
+++ RaceTrack/RaceGame.cs
@@ -9,23 +9,177 @@ namespace RaceTrack;
 
 class RaceGame
 {
-    #region Fullscreen Variables
-    [DllImport("kernel32.dll", ExactSpelling = true)]
-    private static extern IntPtr GetConsoleWindow();
-    private static IntPtr ThisConsole = GetConsoleWindow();
-    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-    private const int HIDE = 0;
-    private const int MAXIMIZE = 3;
-    private const int MINIMIZE = 6;
-    private const int RESTORE = 9;
-    #endregion
+    public const char pWrite = '*';
+    public const char wallV = '║';
+    public const char wallH = '═';
 
-    public static void Main()
+    public static int pX = 80;
+    public static int pY = 25;
+
+    public static bool run = true;
+
+    public static string WallsString =
+        "╔══════════════════════════════════════════════════════════════════════════════════════════════════════╗\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                ╔════════════════════════════════════════════════════════════════════╗                ║\n" +
+        "║                ║                                                                    ║                ║\n" +
+        "║                ║                                                                    ║                ║\n" +
+        "║                ║                                                                    ║                ║\n" +
+        "║                ║                                                                    ║                ║\n" +
+        "║                ║                                                                    ║                ║\n" +
+        "║                ║                                                                    ║                ║\n" +
+        "║                ║                                                                    ║                ║\n" +
+        "║                ║                                                                    ║                ║\n" +
+        "║                ║                                                                    ║                ║\n" +
+        "║                ║                                                                    ║                ║\n" +
+        "║                ║                                                                    ║                ║\n" +
+        "║                ╚════════════════════════════════════════════════════════════════════╝                ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "║                                                                                                      ║\n" +
+        "╚══════════════════════════════════════════════════════════════════════════════════════════════════════╝";
+
+
+    public static void Run()
     {
-        Console.Title = "Race Game";
-        Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
-        ShowWindow(ThisConsole, MAXIMIZE);
+        Draw();
+    }
+
+    public static void Draw()
+    {
+        MapDraw();
+        while (run)
+        {
+            Console.CursorVisible = false;
+            PlayerDraw(pWrite, pX, pY);
+            PlayerMove();
+        }
+    }
+
+    public static void PlayerDraw(char toWrite, int x, int y)
+    {
+        try
+        {
+            if (x >= 0 && y >= 0)
+            {
+                Console.SetCursorPosition(x, y); // DELETE OLD
+                Console.Write(toWrite);
+
+            }
+            else
+            {
+                Die();
+            }
+        }
+        catch (Exception) { }
+    }
+
+    public static void MapDraw()
+    {
+        Console.SetCursorPosition(75, 15);
+        Render(WallsString, false);
+    }
+
+
+
+    public static void Die()
+    {
+        Console.Clear();
+    }
+
+
+    char BoardAt(int x, int y) => WallsString[y * 42 + x];
+
+    bool IsWall(int x, int y) => BoardAt(x, y) is not ' ';
+
+    //bool CanMove(int x, int y, Direction direction) => direction switch
+    //{
+    //    Direction.Up =>
+    //        !IsWall(x - 1, y - 1) &&
+    //        !IsWall(x, y - 1) &&
+    //        !IsWall(x + 1, y - 1),
+    //    Direction.Down =>
+    //        !IsWall(x - 1, y + 1) &&
+    //        !IsWall(x, y + 1) &&
+    //        !IsWall(x + 1, y + 1),
+    //    Direction.Left =>
+    //        x - 2 < 0 || !IsWall(x - 2, y),
+    //    Direction.Right =>
+    //        x + 2 > 40 || !IsWall(x + 2, y),
+    //    _ => throw new NotImplementedException(),
+    //};
+
+    public static void PlayerMove()
+    {
+
+        if (Console.KeyAvailable)
+        {
+            var command = Console.ReadKey().Key;
+
+            switch (command)
+            {
+                case ConsoleKey.DownArrow:
+                    
+                    pY++;
+                    break;
+                case ConsoleKey.UpArrow:
+                    
+                    if (pY > 0)
+                    {
+                        pY--;
+                    }
+                    break;
+                case ConsoleKey.LeftArrow:
+                    
+                    if (pX > 0)
+                    {
+                        pX--;
+                    }
+                    break;
+                case ConsoleKey.RightArrow:
+                    
+                    pX++;
+                    break;
+            }
+
+
+            PlayerDraw(pWrite, pX, pY);
+        }
+
+    }
+
+
+    public static void Render(string @string, bool renderSpace = true)
+    {
+        int x = Console.CursorLeft;
+        int y = Console.CursorTop;
+        foreach (char c in @string)
+        {
+            if (c is '\n')
+            {
+                Console.SetCursorPosition(x, ++y);
+            }
+            else if (c is not ' ' || renderSpace)
+            {
+                Console.Write(c);
+            }
+            else
+            {
+                Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
+            }
+        }
     }
 }
 
--- Test Code Project/Program.cs
+++ Test Code Project/Program.cs
@@ -1,41 +1,16 @@
-﻿class head
-{
+﻿using System;
 
-    public static void Main()
+class Program
+{
+    static void Main(string[] args)
     {
-        double resultat = 0;
-        Console.WriteLine("Velkommen til ConCalc\n" +
-            "---------------------\n" +
-            "1. Plus\n" +
-            "2. Træk fra\n" +
-            "3. Gange\n" +
-            "4. Dividere\n" +
-            "---------------------\n" +
-            "Vælg en funktion: ");
-        int funktion = int.Parse(Console.ReadLine());
-        Console.Write("Tal 1: ");
-        int tal1 = int.Parse(Console.ReadLine());
-        Console.Write("Tal 2: ");
-        int tal2 = int.Parse(Console.ReadLine());
-        switch (funktion)
-        {
-            case 1:
-                Console.Write(tal1 + tal2);
-                break;
-            case 2:
-                Console.Write(tal1 - tal2);
-                break;
-            case 3:
-                Console.Write(tal1 * tal2);
-                break;
-            case 4:
-                Console.Write(tal1 / tal2);
-                break;
-        }
-        Console.WriteLine("---------------------\n" +
-            $"Resultat: {resultat}\n" +
-            "---------------------");
-        Console.ReadKey();
+        int a = 20;
+
+        int b = 10;
+
+        Console.WriteLine(a + b);
     }
 }
+
+
 
