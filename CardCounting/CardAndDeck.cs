2b1d0c32c3e884799e20afd1042f41196d5826ff
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-09-25 04:55:46 +0200

    Card Counting Initial

---
+++ CardCounting/CardAndDeck.cs
@@ -0,0 +1,102 @@
+﻿
+
+using System.Text;
+
+public class Deck
+{
+
+    public Deck()
+    {
+        Cards = MakeNewDeck();
+        DeckID += 1;
+    }
+
+    public int DeckID { get; set; }
+    static readonly Random rng = new Random();
+    List<Card> Cards = new();
+
+    public List<Card> MakeNewDeck()
+    {
+        int cardsInASuit = 13;
+
+        
+
+        foreach (Suits currentSuit in Enum.GetValues(typeof(Suits)))
+        {
+            for (int i = 1; i <= cardsInASuit; i++)
+            {
+                Cards.Add(new Card(i, currentSuit));
+            }
+        }
+        return Cards;
+    }
+
+    
+
+    public string NameOfCard(Card card)
+    {
+        string name;
+
+        switch (card.NumberOnCard)
+        {
+            case 1:
+                name = "Ace";
+                break;
+            case 11:
+                name = "Jack";
+                break;
+            case 12:
+                name = "Queen";
+                break;
+            case 13:
+                name = "King";
+                break;
+            default:
+                name = card.NumberOnCard.ToString();
+                break;
+        }
+
+        return name;
+    }
+
+    // How it's printed
+    public override string ToString()
+    {
+        const int spaceBetween = 12;
+        var sb = new StringBuilder();
+        int index = 0;
+        while (index < Cards.Count)
+        {
+            sb.Append($"{NameOfCard(Cards[index])} of {Cards[index].SuitOnCard}    ");
+            index++;
+            if (index % 4 == 0)
+            {
+                sb.AppendLine();
+                sb.AppendLine();
+            }
+        }
+        return sb.ToString();
+    }
+}
+
+public class Card
+{
+    public Card(int number, Suits suit)
+    {
+        NumberOnCard = number;
+        SuitOnCard = suit;
+    }
+
+    public int NumberOnCard { get; }
+
+    public Suits SuitOnCard { get; }
+
+}
+
+public enum Suits
+{
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
+}
---
+++ CardCounting/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+public class Program
+{
+    public static void Main()
+    {
+        Deck deck1 = new Deck();
+        Deck deck2 = new Deck();
+        List<Deck> Decks = new List<Deck>();
+
+
+
+        Console.WriteLine(deck1);
+        Console.WriteLine();
+        Console.WriteLine(deck2);
+    }
+}
