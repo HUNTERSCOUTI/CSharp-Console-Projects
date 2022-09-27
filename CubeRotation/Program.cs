e44cf11517000414ed0d4ee1930b22eb5d4179ce
Zilas Jørgensen <Zilas2004@gmail.com>
2022-09-27 13:19:08 +0200

    CubeRotation Initial

---
+++ CubeRotation/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Math;
+
+class Program
+{
+    static float A, B, C;
+    static float cubeWidth = 10;
+    static int width = 160;
+    static int height = 44;
+    static float[] zBuffer = new float[160 * 40];
+    static char[] charBuffer = new char[160 * 44];
+    int backgroundASCIIcode = ' ';
+
+    public static void Main()
+    {
+        while (true)
+        {
+            
+        }
+    }
+
+    public static float CalculateX(int i, int j, int k)
+    {
+        return (float)(j * Sin(A) * Sin(B) * Cos(C) - k * Cos(A) * Sin(B) * Cos(C) + j * Cos(A) * Sin(C) + k * Sin(A) * Sin(C) + i * Cos(B) * Cos(C));
+    }
+
+    public static float CalculateY(int i, int j, int k)
+    {
+        return (float)(j * Cos(A) * Cos(C) - j * Sin(A) * Sin(B) * Sin(C) + k * Cos(A) * Sin(B) * Sin(C) - i * Cos(B) * Sin(C));
+    }
+
+    public static float CalculateZ(int i, int j, int k)
+    {
+        return (float)(k * Cos(A) * Cos(B) - j * Sin(A) * Cos(B) + i * Sin(B));
+    }
+}
