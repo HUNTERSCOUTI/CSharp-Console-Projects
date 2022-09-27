c2dc4573e912307bf56e22c1ef9ad5a4663ee3a0
Zilas Jørgensen <Zilas2004@gmail.com>
2022-09-27 08:31:15 +0200

    Laptop 1.2

---
+++ WineSys/WineSysMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WineSys;
+
+class WineSysMain
+{
+    public static void Main()
+    {
+        int WindowWidth = 100;
+        int WindowHeight = 35;
+
+        Console.Title = "Wine Sys";
+        Console.SetWindowSize(WindowWidth, WindowHeight);
+    }
+}
