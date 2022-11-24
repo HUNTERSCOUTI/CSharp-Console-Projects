20acc5095023d9aea73d40fafe187928805a61cb
zilas jørgensen <zilas2004@gmail.com>
2022-11-24 14:59:18 +0100

    Test Code + Kata Start

--- Test Code Project/Program.cs
+++ Test Code Project/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Test");
+﻿using System;
+using System.Text;
+
+public class Kata
+{
+    public static void Main()
+    {
+        Console.WriteLine(Likes(new string[0]));
+        Console.WriteLine(Likes(new string[] { "Peter" }));
+        Console.WriteLine(Likes(new string[] { "Jacob", "Alex" }));
+        Console.WriteLine(Likes(new string[] { "Max", "John", "Mark" }));
+        Console.WriteLine(Likes(new string[] { "Alex", "Jacob", "Mark", "Max" }));
+    }
+
+    public static string Likes(string[] name)
+    {
+        
+    }
+}
