414887c6554d659ecd8a20031f4d5d0e0746cd64
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-12-08 22:38:25 +0100

    Advent Of Code Day 1

---
+++ Advent Of Code/Day 1/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    /*
+    public static void Main()
+    {
+        string[] lines = File.ReadAllLines("C:\\Users\\ZilasJ\\source\\repos\\CSharp-Console-Projects\\Advent Of Code\\Day 1\\Calories.txt").ToArray();
+        int highestNum = 0;
+        int newNum = 0;
+
+        foreach (string line in lines)
+        {
+            if (line != string.Empty)
+            {
+                newNum += int.Parse(line);
+            }
+            else 
+            {
+                if (newNum > highestNum)
+                    highestNum = newNum;
+                newNum = 0;
+            }
+        }
+        Console.WriteLine(highestNum);
+    }*/
+
+    public static void Main()
+    {
+
+    }
+}
+
