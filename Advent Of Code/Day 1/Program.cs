8ee3c4ee38906ef331042bf5415b926305da33e6
zilas jørgensen <zilas2004@gmail.com>
2022-12-13 12:46:16 +0100

    AOC Day 1 & Day 2

--- Advent Of Code/Day 1/Program.cs
+++ Advent Of Code/Day 1/Program.cs
@@ -1,9 +1,11 @@
-﻿public class Program
+﻿using System;
+
+public class Program
 {
-    /*
+    /* Day 1, Puzzle 1
     public static void Main()
     {
-        string[] lines = File.ReadAllLines("C:\\Users\\ZilasJ\\source\\repos\\CSharp-Console-Projects\\Advent Of Code\\Day 1\\Calories.txt").ToArray();
+        string[] lines = File.ReadAllLines("C:\\Users\\zilas\\source\\repos\\CSharp-Console-Projects\\Advent Of Code\\Day 1\\Calories.txt").ToArray();
         int highestNum = 0;
         int newNum = 0;
 
@@ -23,9 +25,28 @@
         Console.WriteLine(highestNum);
     }*/
 
+    /* Day 1, Puzzle 2
     public static void Main()
     {
+        string[] lines = File.ReadAllLines("C:\\Users\\zilas\\source\\repos\\CSharp-Console-Projects\\Advent Of Code\\Day 1\\Calories.txt").ToArray();
 
+        List<int> nums = new();
+        int section = 0;
+
+        foreach (string line in lines)
+        {
+            if (line != string.Empty)
+            {
+                section += int.Parse(line);
+            } else
+            {
+                nums.Add(section);
+                section = 0;
+            }
+        }
+
+        Console.WriteLine(nums.Max() + nums.OrderByDescending(z => z).Skip(1).First() + nums.OrderByDescending(z => z).Skip(2).First());
     }
+    */
 }
 
---
+++ Advent Of Code/Day 2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code.Day_2;
+
+public class Program
+{
+    /* Day 2, Puzzle 1
+    public static void Main()
+    {
+        string[] input = File.ReadAllLines("C:\\Users\\zilas\\source\\repos\\CSharp-Console-Projects\\Advent Of Code\\Day 2\\RPSInput.txt").ToArray();
+
+        // A / X = ROCK
+        // B / Y = PAPER
+        // C / Z = SCISSORS
+
+        int totalScore = 0;
+
+        foreach (var line in input)
+        {
+            int pointsGained = 0;
+            switch (line[2])
+            {
+                case 'X':
+                    pointsGained += 1;
+                    break;
+                case 'Y':
+                    pointsGained += 2;
+                    break;
+                case 'Z':
+                    pointsGained += 3;
+                    break;
+            }
+            switch (line[0], line[2])
+            {
+                //Win
+                case ('A', 'Y'):
+                case ('B', 'Z'):
+                case ('C', 'X'):
+                    pointsGained += 6;
+                    break;
+                //DRAW
+                case ('A', 'X'):
+                case ('B', 'Y'):
+                case ('C', 'Z'):
+                    pointsGained += 3;
+                    break;
+                //LOSE
+                default:
+
+                    break;
+            }
+            totalScore = totalScore + pointsGained;
+        }
+        Console.WriteLine(totalScore);
+    }
+    */
+    /*
+    public static void Main()
+    {
+        string[] input = File.ReadAllLines("C:\\Users\\zilas\\source\\repos\\CSharp-Console-Projects\\Advent Of Code\\Day 2\\RPSInput.txt").ToArray();
+
+        int totalScore = 0;
+
+        // X = LOSE
+        // Y = DRAW
+        // Z = WIN
+
+        foreach(var line in input)
+        {
+
+        }
+
+        Console.WriteLine(totalScore);
+    }
+    */
+
+    public static void Main()
+    {
+        ReadWeb();
+    }
+
+    public async static Task ReadWeb()
+    {
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "C# console program");
+
+        var content = await client.GetStringAsync("https://adventofcode.com/2022/day/2/input");
+
+        Console.WriteLine(content);
+    }
+}
